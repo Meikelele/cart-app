0d2fce23fcfcf75256916a0ae0618d4ff90e777c
Meikelele <michal.bak2002@interia.pl>
2025-05-31 14:03:38 +0200

    springboot initial

---
+++ cart-app/src/main/java/com/example/cartApp/CartAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartAppApplication.class, args);
+	}
+
+}
---
+++ cart-app/src/test/java/com/example/cartApp/CartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
