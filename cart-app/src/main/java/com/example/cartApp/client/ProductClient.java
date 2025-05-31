2dd666b300b874f3de4ae73d523e508022a1dcd7
Meikelele <michal.bak2002@interia.pl>
2025-05-31 15:26:04 +0200

    added method to add product to cart and delete product from cart

---
+++ cart-app/src/main/java/com/example/cartApp/client/ProductClient.java
@@ -0,0 +1,35 @@
+package com.example.cartApp.client;
+
+import com.example.cartApp.model.ProductDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+public class ProductClient {
+
+    private final RestTemplate restTemplate;
+    private final String productServiceUrl;
+
+    public ProductClient(RestTemplate restTemplate, @Value("${product.service.url}") String productServiceUrl) {
+        this.restTemplate = restTemplate;
+        this.productServiceUrl = productServiceUrl;
+    }
+
+    /**
+     * Wyciagam produkt po jego Id (jako String).
+     */
+    public ProductDto getProductById(UUID productId) {
+        String url = productServiceUrl + "/" + productId.toString();
+        try {
+            ResponseEntity<ProductDto> response = restTemplate.getForEntity(url, ProductDto.class);
+            return response.getBody();
+        } catch (RestClientException ex) {
+            throw new RuntimeException("Nie udało się pobrać produktu z serwisu: " + ex.getMessage());
+        }
+    }
+}
